2cff7fc1a0f94c5700d087f62ca2f39521bc01a2
KenlikDev <kenlik.dev@proton.me>
2026-09-21 22:53:39 +0300

    style(cart): satisfy repository ktlint formatting

--- server/cart/src/main/kotlin/com/kenlikdev/qmarket/cart/repository/CartRepository.kt
+++ server/cart/src/main/kotlin/com/kenlikdev/qmarket/cart/repository/CartRepository.kt
@@ -1,10 +1,10 @@
 package com.kenlikdev.qmarket.cart.repository
 
-import jakarta.persistence.LockModeType
 import com.kenlikdev.qmarket.cart.domain.Cart
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import java.util.UUID
