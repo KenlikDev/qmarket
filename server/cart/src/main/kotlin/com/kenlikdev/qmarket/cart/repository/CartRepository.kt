ef82e769cf281a41fcc8d5836bf2c691f9d06929
KenlikDev <kenlik.dev@proton.me>
2026-09-21 22:32:08 +0300

    fix(cart): restore ktlint import ordering

--- server/cart/src/main/kotlin/com/kenlikdev/qmarket/cart/repository/CartRepository.kt
+++ server/cart/src/main/kotlin/com/kenlikdev/qmarket/cart/repository/CartRepository.kt
@@ -1,7 +1,7 @@
 package com.kenlikdev.qmarket.cart.repository
 
-import jakarta.persistence.LockModeType
 import com.kenlikdev.qmarket.cart.domain.Cart
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Lock
